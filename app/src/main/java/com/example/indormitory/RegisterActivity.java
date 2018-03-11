b4a5a5afc13d62601c30f4509cfb125abe59c094
Rostyslav <rostyslavkhlan@gmail.com>
2018-03-12 01:49:57 +0200

    add empty register activity

---
+++ app/src/main/java/com/example/indormitory/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.indormitory;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ростислав on 12.03.2018.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
