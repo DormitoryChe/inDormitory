9cae40b383fdcda7aba8b82d6180ae055def6b45
Rostyslav <rostyslavkhlan@gmail.com>
2018-03-12 15:22:16 +0200

    update navigation menu

--- app/src/main/java/com/example/indormitory/RegisterActivity.java
+++ app/src/main/java/com/example/indormitory/RegisterActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class RegisterActivity extends AppCompatActivity {
+    private String name;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
