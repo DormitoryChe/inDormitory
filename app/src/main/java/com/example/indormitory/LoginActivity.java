e2cbbdca5298c268fbe1fa86b7d53946370ae8fc
vproh <proh8530@gmail.com>
2018-03-13 15:41:55 +0200

    ** add more description animation (not finished) ** add login activity validator

--- app/src/main/java/com/example/indormitory/LoginActivity.java
+++ app/src/main/java/com/example/indormitory/LoginActivity.java
@@ -5,24 +5,58 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.indormitory.validators.LoginValidator;
 
 /**
  * Created by vproh on 11.03.2018.
  */
 
 public class LoginActivity extends AppCompatActivity {
-    private TextView registerTextView;
+    private TextView mRegisterTextView;
+    private TextView mLoginInput;
+    private TextView mPasswordInput;
+    private Button mSubmitButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        registerTextView = findViewById(R.id.register_now);
-        registerTextView.setOnClickListener(new View.OnClickListener() {
+        mSubmitButton = findViewById(R.id.login_submit_button);
+        mRegisterTextView = findViewById(R.id.register_now);
+        mLoginInput = findViewById(R.id.login_email_input);
+        mPasswordInput = findViewById(R.id.login_password_input);
+
+        mRegisterTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int loginFieldLength = mLoginInput.getText().length();
+                int passwordFieldLength = mPasswordInput.getText().length();
+
+                if(LoginValidator.isPasswordFieldEmpty(passwordFieldLength)) {
+                    mPasswordInput.setError(getString(R.string.empty_login_password_error));
+                } else if(!LoginValidator.isPasswordLengthCorrect(passwordFieldLength)) {
+                    mPasswordInput.setError(getString(R.string.incorrect_login_password_error));
+                }
+
+                if(LoginValidator.isEmailFieldEmpty(loginFieldLength)) {
+                    mLoginInput.setError(getString(R.string.empty_login_email_error));
+                } else if(!LoginValidator.isEmailValid(mLoginInput.getText().toString())) {
+                    mLoginInput.setError(getString(R.string.email_not_valid));
+                } else if(!LoginValidator.isEmailLenghtCorrect(loginFieldLength)) {
+                    mLoginInput.setError(getString(R.string.incorrect_login_email_error));
+                }
             }
         });
     }
---
+++ app/src/main/java/com/example/indormitory/validators/LoginValidator.java
@@ -0,0 +1,36 @@
+package com.example.indormitory.validators;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by vproh on 13.03.2018.
+ */
+
+ public class LoginValidator {
+    private static final String EMAIL_REGEX = "^\\w+([\\.-]?\\w+){1,3}@(\\w+\\.\\w{2,3})$";
+    private static final int MIN_LOGIN_INPUT_CHARS = 5;
+    private static final int MIN_PASSWORD_INPUT_CHARS = 6;
+
+    public static boolean isEmailValid(String email) {
+        Pattern emailPattern = Pattern.compile(EMAIL_REGEX);
+
+        return emailPattern.matcher(email).matches();
+    }
+
+    public static  boolean isEmailFieldEmpty (int emailLen) {
+        return emailLen == 0;
+    }
+    public static boolean isEmailLenghtCorrect (int emailLen) {
+        return !(emailLen < MIN_LOGIN_INPUT_CHARS);
+    }
+
+    public static boolean isPasswordLengthCorrect (int passwordLen) {
+        return !(passwordLen < MIN_PASSWORD_INPUT_CHARS);
+    }
+
+    public  static boolean isPasswordFieldEmpty (int passwordLen) {
+        return passwordLen == 0;
+    }
+
+}
